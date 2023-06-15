b3fdc64d64e62cad726ddd14d35b096f80d18dc7
Noel Rivas <noel@matrushka.com.mx>
2023-06-15 15:43:54 -0600

    Implemented the content loader.

--- index.ts
+++ index.ts
@@ -5,11 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import ContentLoader from './src/content-loader';
 
-// Start the store
-// Import the data loader and get the loading started;
-// connect the loading event to the store actions
-
-
+ContentLoader.loadAll();
 
 AppRegistry.registerComponent(appName, () => App);
--- src/content-loader.ts
+++ src/content-loader.ts
@@ -1,5 +1,59 @@
+import store from './redux/store';
+import { Actions } from './redux/actions';
+
+
 // Set the content URLs
 // Load the contents and get a promise for each
 // On promise.all dispatch the actions to populate
 // the store with the content, and to set the state
 // to "ready" so the app renders
+
+export default class ContentLoader {
+  static loadCollections() {
+    const collectionsUrl = 'https://note-to-self-app.s3.us-west-2.amazonaws.com/collections.json';
+    return fetch(collectionsUrl)
+      .then((response) => {
+        if(!response.ok) {
+          throw new Error('The Collections request failed');
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        store.dispatch(Actions.collectionsAddMultiple(data.collections));
+        return data;
+      })
+      .catch((error) => {
+        // TODO: figure out a better way to handle errors in React Native
+        console.error(`Unhandled error: ${error.message}`);
+      })
+  }
+
+  static loadNotes() {
+    const notesUrl = 'https://note-to-self-app.s3.us-west-2.amazonaws.com/notes.json';
+    return fetch(notesUrl)
+      .then((response) => {
+        if(!response.ok) {
+          throw new Error('The Notes request failed');
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        store.dispatch(Actions.notesAddMultiple(data.notes));
+        return data;
+      })
+      .catch((error) => {
+        // TODO: figure out a better way to handle errors in React Native
+        console.error(`Unhandled error: ${error.message}`);
+      })
+  }
+
+  static loadAll() {
+    return Promise.all([ContentLoader.loadCollections(), ContentLoader.loadNotes()])
+      .then((values) => {
+        store.dispatch(Actions.appSetLoaded());
+        return values;
+      })
+  }
+};
