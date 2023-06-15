34a5480b9f2d57bf2d541a1b57bb5ca731545467
Noel Rivas <noel@matrushka.com.mx>
2023-06-15 12:19:02 -0600

    Finished writing tests for the store actions.

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -7,6 +7,7 @@ import React from 'react';
 import App from '../App';
 import store from '../src/redux/store';
 import { Actions } from '../src/redux/actions';
+import sampleNotes from '../sample-data/notes.json';
 import sampleCollections from '../sample-data/collections.json';
 
 // Note: test renderer must be required after react-native.
@@ -16,7 +17,7 @@ it('renders correctly', () => {
   renderer.create(<App />);
 });
 
-it('State switches loaded on ActionAppSetLoaded', () => {
+it('has the "loaded" state set on ActionAppSetLoaded', () => {
   const state_before = store.getState();
   expect(state_before.loaded).toEqual(false);
 
@@ -25,15 +26,47 @@ it('State switches loaded on ActionAppSetLoaded', () => {
   expect(state_after.loaded).toEqual(true);
 });
 
-it('State notes added on ActionNotesAddMultiple', () => {
-  
+it('has notes added to state on ActionNotesAddMultiple', () => {
+  const state_before = store.getState();
+  expect(state_before.notes.length).toEqual(0);
+
+  const notes = sampleNotes.notes;
+
+  store.dispatch(Actions.notesAddMultiple(notes));
+  const state_after = store.getState();
+  expect(state_after.notes.length).toEqual(3);
 });
 
-it('State collections added on ActionCollectionsAddMultiple', () => {
-  // const state_before = store.getState();
-  // expect(state_before.collections.length).toEqual(0);
+it('has active note set on ActionNoteSetActive', () => {
+  const state_before = store.getState();
+  expect(state_before.activeNote).toBeUndefined();
 
-  // store.dispatch(Actions.collectionsAddMultiple(collections));
-  // const state_after = store.getState();
-  // expect(state_after.loaded).toEqual(true);
+  const note = sampleNotes.notes[0];
+
+  store.dispatch(Actions.notesAddMultiple(note));
+  const state_after = store.getState();
+  expect(state_after.activeNote).toEqual(note);
+});
+
+it('has collections added to state on ActionCollectionsAddMultiple', () => {
+  const state_before = store.getState();
+  expect(state_before.collections.length).toEqual(0);
+
+  const collections = sampleCollections.collections;
+
+  store.dispatch(Actions.collectionsAddMultiple(collections));
+  const state_after = store.getState();
+  expect(state_after.collections.length).toEqual(3);
+});
+
+
+it('has active collection set on ActionCollectionSetActive', () => {
+  const state_before = store.getState();
+  expect(state_before.activeCollection).toBeUndefined();
+
+  const collection = sampleCollections.collections[0];
+
+  store.dispatch(Actions.collectionSetActive(collection));
+  const state_after = store.getState();
+  expect(state_after.activeCollection).toEqual(collection);
 });
