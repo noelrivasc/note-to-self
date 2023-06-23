411568222048e29e347042222638211a82fd3157
Noel Rivas <noel@matrushka.com.mx>
2023-06-22 21:09:44 -0600

    Made the History and Settings placeholders less hideous.

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { 
+  View, 
+  Text,
+  StyleSheet,
+} from 'react-native';
+import theme from '../globals/theme';
 
 export default class Settings extends Component {
   render() {
+    const stylesheet = StyleSheet.create({
+      historyContainer: {
+        padding: 20,
+      },
+      text: {
+        color: theme.palette.neutral,
+        fontSize: 24,
+        marginBottom: 12,
+      }
+    });
+
     return (
-      <View>
-        <Text>Wait for me. I am History.</Text>
+      <View style={ stylesheet.historyContainer } >
+        <Text style={ stylesheet.text } >This screen will eventually contain the 10 most recently viewed notes.</Text>
       </View>
     );
   }
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { 
+  View, 
+  Text,
+  StyleSheet,
+} from 'react-native';
+import theme from '../globals/theme';
 
 export default class Settings extends Component {
   render() {
+    const stylesheet = StyleSheet.create({
+      settingsContainer: {
+        padding: 20,
+      },
+      text: {
+        color: theme.palette.neutral,
+        fontSize: 24,
+        marginBottom: 12,
+      }
+    });
+
     return (
-      <View>
-        <Text>Wait for me. I am Settings.</Text>
+      <View style={ stylesheet.settingsContainer } >
+        <Text style={ stylesheet.text } >This screen will eventually be the settings form.</Text>
+        <Text style={ stylesheet.text } >— Reload content & set content source(s).</Text>
+        <Text style={ stylesheet.text } >— Set semi-random reminders.</Text>
       </View>
     );
   }
